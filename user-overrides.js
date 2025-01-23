8e76babaadf69b5047d4ffe4a013f9fccf2cd75f
Gts <gts@fermion.org>
2025-01-22 19:49:29 -0500

    Fixed missing semicolons

--- user-overrides.js
+++ user-overrides.js
@@ -1,8 +1,8 @@
 // Overrides
 user_pref("extensions.pocket.enabled", false); // (????) I do not use it
-user_pref("browser.preferences.moreFromMozilla", false) // (????) It is just ads for mozilla products
+user_pref("browser.preferences.moreFromMozilla", false); // (????) It is just ads for mozilla products
 
-user_pref("browser.safebrowsing.downloads.remote.enabled", true) // (0402) I am fine with giving a little info about the file to google
+user_pref("browser.safebrowsing.downloads.remote.enabled", true); // (0402) I am fine with giving a little info about the file to google
 
 // Restore session
 user_pref("browser.startup.page", 3); // (0102) Restore session on startup
